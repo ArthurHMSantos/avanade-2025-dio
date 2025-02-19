f01cdffaa076c0e0bf77323364b5b647dd520922
Arthur Henrique <arthurmsantos2002@gmail.com>
2025-02-18 22:15:53 -0300

    mover pro correto

--- src/main/java/lab/dio/Avanade2025Application.java
+++ src/main/java/lab/dio/Avanade2025Application.java
@@ -1,8 +1,13 @@
-package lab.dio.avanade;
+package lab.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Avanade2025Application {
 
